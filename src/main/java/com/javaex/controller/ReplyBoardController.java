1fac4e0e5891b7f945d3b139bf5e9f470bc69ed8
양지선 <@DESKTOP-PGT6R8G>
2020-07-12 18:00:39 +0900

    코드정리

--- src/main/java/com/javaex/controller/ReplyBoardController.java
+++ src/main/java/com/javaex/controller/ReplyBoardController.java
@@ -21,92 +21,90 @@ import oracle.jdbc.proxy.annotation.GetDelegate;
 @Controller
 @RequestMapping("board")
 public class ReplyBoardController {
-	
+
 	@Autowired
 	ReplyBoardService boardService;
-	
-	@RequestMapping("/search") 
-	public String search(@RequestParam("keyword")String keyword,
-			@RequestParam("pg") int pg, 
-			Model model) { 
-		Paging pgVo = new Paging(5, 5, boardService.keywordAllPage(keyword),pg);
-		
-		List<ReplyBoardVo> bList = boardService.search(pgVo.getWriting_Start(), pgVo.getWriting_End(),keyword);
-		model.addAttribute("pg", pgVo); 
-		model.addAttribute("bList", bList); 
-		return "board/list"; 
-	}
-	
-	@RequestMapping("/list")
-	public String list(Model model, @RequestParam("pg") int pg) {
-		
-		Paging pgVo = new Paging(10, 10, boardService.allPage(),pg);
-		
-		List<ReplyBoardVo> bList = boardService.list(pgVo.getWriting_Start(), pgVo.getWriting_End());
-		
+
+	@RequestMapping("/search")
+	public String search(@RequestParam("keyword") String keyword, 
+						 @RequestParam("pg") int pg, 
+						 Model model) {
+		Paging pgVo = new Paging(5, 5, boardService.keywordAllPage(keyword), pg);
+
+		List<ReplyBoardVo> bList = boardService.search(pgVo.getWriting_Start(), pgVo.getWriting_End(), keyword);
 		model.addAttribute("pg", pgVo);
 		model.addAttribute("bList", bList);
-		return "board/list";       
+		return "board/list";
 	}
-	
+
+	@RequestMapping("/list")
+	public String list(Model model, @RequestParam("pg") int pg) {
+
+		Paging pgVo = new Paging(10, 10, boardService.allPage(), pg);
+
+		List<ReplyBoardVo> bList = boardService.list(pgVo.getWriting_Start(), pgVo.getWriting_End());
+
+		model.addAttribute("pg", pgVo);
+		model.addAttribute("bList", bList);
+		return "board/list";
+	}
+
 	@RequestMapping("/read")
 	public String read(@RequestParam("no") int no, Model model) {
 		model.addAttribute("getBoard", boardService.getBoard(no));
-		
+
 		return "board/read";
 	}
-	
+
 	@RequestMapping("/modifyForm")
 	public String modifyForm(@RequestParam("no") int no, Model model) {
 		model.addAttribute("getBoard", boardService.getBoard(no));
 		return "board/modifyForm";
 	}
-	
+
 	@RequestMapping("/modifyAction")
 	public String modifyAction(@ModelAttribute ReplyBoardVo boardVo, Model model) {
-		
+
 		model.addAttribute("no", boardService.updateBoard(boardVo));
 		return "redirect:/board/list?pg=1";
 	}
-	
+
 	@RequestMapping("/writeForm")
 	public String writeForm() {
 		return "board/writeForm";
 	}
-	
+
 	@RequestMapping("/writeAction")
 	public String writeAction(@ModelAttribute ReplyBoardVo boardVo) {
 		boardService.insertBoard(boardVo);
 		return "redirect:/board/list?pg=1";
 	}
-	
+
 	@RequestMapping("/delete")
 	public String delete(@ModelAttribute ReplyBoardVo boardVo) {
 		boardService.deleteBoard(boardVo);
 		return "redirect:/board/list?pg=1";
 	}
-	
-		
-	@RequestMapping("/replyWriteForm") 
-	public String replyWriteForm(@RequestParam("group_no")int group_no,
+
+	@RequestMapping("/replyWriteForm")
+	public String replyWriteForm(@RequestParam("group_no") int group_no, 
 								 @RequestParam("boardType") String boardType,
-								 @RequestParam("parentsNo") int parentsNo,
+								 @RequestParam("parentsNo") int parentsNo, 
 								 Model model) {
-	  
-	  Map<String, Object> noTypeMap= new HashMap<String, Object>();
-	  noTypeMap.put("group_no", group_no); 
-	  noTypeMap.put("boardType", boardType);
-	  model.addAttribute("board", noTypeMap);
-	  model.addAttribute("parentsNo", parentsNo);
-	  
-	  return "board/writeForm"; 
+
+		Map<String, Object> noTypeMap = new HashMap<String, Object>();
+		noTypeMap.put("group_no", group_no);
+		noTypeMap.put("boardType", boardType);
+		model.addAttribute("board", noTypeMap);
+		model.addAttribute("parentsNo", parentsNo);
+
+		return "board/writeForm";
 	}
-	  
-	@RequestMapping("replyWriteAction") 
+
+	@RequestMapping("replyWriteAction")
 	public String replyWriteAction(@ModelAttribute ReplyBoardVo replyVo) {
 		boardService.replyInsert(replyVo);
-		return "redirect:/board/list?pg=1"; 
-	 }
-	 
-	
+		return "redirect:/board/list?pg=1";
+	}
+
 }
--- src/main/java/com/javaex/service/ReplyBoardService.java
+++ src/main/java/com/javaex/service/ReplyBoardService.java
@@ -13,17 +13,17 @@ import com.javaex.vo.ReplyBoardVo;
 
 @Service
 public class ReplyBoardService {
-	
+
 	@Autowired
 	ReplyBoardDao boardDao;
-	
+
 	public List<ReplyBoardVo> list(int start, int end) {
-		
+
 		Map<String, Object> pageMap = new HashMap<String, Object>();
 		pageMap.put("start", start);
 		pageMap.put("end", end);
 		pageMap.put("keyword", "");
-		
+
 		List<ReplyBoardVo> bList = boardDao.search(pageMap);
 
 		return bList;
@@ -34,54 +34,54 @@ public class ReplyBoardService {
 		pageMap.put("start", start);
 		pageMap.put("end", end);
 		pageMap.put("keyword", keyword);
-		
+
 		List<ReplyBoardVo> bList = boardDao.search(pageMap);
 		return bList;
 	}
-	
+
 	public int allPage() {
 		String keyword = "";
 		return boardDao.keywordAllPage(keyword);
 	}
-	
+
 	public int keywordAllPage(String keyword) {
 		return boardDao.keywordAllPage(keyword);
 	}
-	
+
 	public ReplyBoardVo getBoard(int no) {
 		boardDao.hitUpdate(no);
 		return boardDao.getBoard(no);
 	}
-	
+
 	public int updateBoard(ReplyBoardVo vo) {
 		boardDao.updateBoard(vo);
 		return vo.getNo();
 	}
-	
+
 	public int insertBoard(ReplyBoardVo boardVo) {
 		return boardDao.insertBoard(boardVo);
 	}
-	
+
 	public int deleteBoard(ReplyBoardVo boardVo) {
-		
-		  if(boardDao.groupNoDelY(boardVo.getGroup_no()) == 1) {
-			  //같은 그룹의 del이 자기자신 빼고 모두 y일 경우 같은 그룹 다 삭제;
-			  return boardDao.groupAllDel(boardVo.getGroup_no());
-			  
-		  }else if(boardDao.childCount(boardVo.getNo()) == 0 ) {
-			  //자식이 없을 경우 삭제;
-			  return  boardDao.deleteBoard(boardVo); 
-			  
-		  }else {
-			  //자식이 있음;
-			  return boardDao.delUpdate(boardVo);
-		  }
+
+		if (boardDao.groupNoDelY(boardVo.getGroup_no()) == 1) {
+			// 같은 그룹의 del이 자기자신 빼고 모두 y일 경우 같은 그룹 다 삭제;
+			return boardDao.groupAllDel(boardVo.getGroup_no());
+
+		} else if (boardDao.childCount(boardVo.getNo()) == 0) {
+			// 자식이 없을 경우 삭제;
+			return boardDao.deleteBoard(boardVo);
+
+		} else {
+			// 자식이 있음;
+			return boardDao.delUpdate(boardVo);
+		}
 	}
-	
+
 	public int replyInsert(ReplyBoardVo vo) {
 		vo.setOrder_no(vo.getOrder_no() + 1);
 		vo.setDepth(vo.getDepth() + 1);
-		
+
 		return boardDao.replyInsert(vo);
 	}
 }
